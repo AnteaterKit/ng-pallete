f84301bf6b98a512e94ca8e5078471fdecaaf647
AnteaterKit <ashatilovdev@gmail.com>
2020-11-30 01:41:59 +0300

    ш

--- src/app/components/input/input.directive.ts
+++ src/app/components/input/input.directive.ts
@@ -1,5 +1,7 @@
-import { Directive, OnInit, Input, Optional, Self, Renderer2, ElementRef, TemplateRef } from '@angular/core';
+import { Directive, OnInit, Input, Optional, Self, Renderer2, ElementRef, TemplateRef, SimpleChanges, OnChanges, OnDestroy } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 export type InputSize = 'large' | 'default' | 'small';
 
@@ -7,11 +9,13 @@ export type InputSize = 'large' | 'default' | 'small';
   selector: 'input[sh-input]',
   exportAs: 'shInput',
   host: {
+    '[class.input-disabled]': 'disabled',
     '[class.input-lg]': `size === 'large'`,
     '[class.input-sm]': `size === 'small'`,
+    '[attr.disabled]': 'disabled || null'
   }
 })
-export class InputDirective implements OnInit {
+export class InputDirective implements OnInit, OnChanges, OnDestroy {
   @Input() size: InputSize = 'default';
 
   @Input()
@@ -25,12 +29,38 @@ export class InputDirective implements OnInit {
     this._disabled = value != null && `${value}` !== 'false';
   }
   _disabled = false;
+  disabled$ = new Subject<boolean>();
+  private destroy$ = new Subject<void>();
+
 
   constructor(@Optional() @Self() public ngControl: NgControl, renderer: Renderer2, elementRef: ElementRef) {
     renderer.addClass(elementRef.nativeElement, 'sh-input');
   }
 
   ngOnInit() {
+    if (this.ngControl) {
+      this.ngControl.statusChanges
+        ?.pipe(
+          filter(() => this.ngControl.disabled !== null),
+          takeUntil(this.destroy$)
+        )
+        .subscribe(() => {
+          // tslint:disable-next-line:no-non-null-assertion
+          this.disabled$.next(this.ngControl.disabled!);
+        });
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { disabled } = changes;
+    if (disabled) {
+      this.disabled$.next(this.disabled);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
--- src/app/modules/input/input.component.ts
+++ src/app/modules/input/input.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterViewInit {
   sizeSm = 'small';
   sizeLg = 'large';
+  @ViewChild('iframe') iframe: ElementRef;
+  @Input() gistId = '7e1c958044a7f6a44f0b5bc2395769cb';
+  @Input() file: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    const fileName = (this.file) ? this.file : '';
+    this.iframe.nativeElement.id = 'gist-' + this.gistId;
+    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const content = `
+        <html>
+        <head>
+          <base target="_parent">
+        </head>
+        <body onload="parent.document.getElementById('${this.iframe.nativeElement.id}')">
+        <script type="text/javascript" src="https://gist.github.com/${this.gistId}.js?file=${fileName}"></script>
+        </body>
+      </html>
+    `;
+    doc.open();
+    doc.write(content);
+    doc.close();
   }
 
 }
