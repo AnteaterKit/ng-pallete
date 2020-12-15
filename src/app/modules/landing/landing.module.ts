65fe8cc2160909ff0393ffb661ab858633ed5289
AnteaterKit <ashatilovdev@gmail.com>
2020-12-15 23:53:23 +0300

    uikit

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing.routing.module';
+import { ButtonModule } from 'src/app/components/button/button.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    ButtonModule,
     LandingRoutingModule
   ],
   exports: [LandingComponent],
